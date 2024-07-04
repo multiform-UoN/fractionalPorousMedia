a133604b6f21ebc4043255b9a734c88134665e8a
Matteo Icardi <mat.icardi@gmail.com>
2024-07-04 19:40:15 +0100

    python code works!

--- python/main_BassetEqn.py
+++ python/main_BassetEqn.py
@@ -1,37 +1,54 @@
+# %%
+# Importing
 import numpy as np
 from scipy import sparse
 from scipy.special import gamma
 import datetime
 
+# %%
+# Parameters
 
-b_fun = lambda k, alpha: (np.power(k+1, 1-alpha)-np.power(k, 1-alpha))/gamma(2-alpha)
-
-f_fun = lambda t, x: np.outer((np.abs(xR-x)*np.exp(x))*(0.3 + np.sin(15*x)/4), np.exp(-t*0.5*np.sin(5*t)))
-# f_fun = lambda t, x: np.outer(0.0*x, 0.0*t)
-
+# Domain
 xL = 0 # Domain left boundary
 xR = 1.0 # Domain right boundary
-N_space = 2001 # Number of space steps
-advection = "upwind" # or "central" or "blended"
 T = 1 # Final time
-N_time = 1001 # Number of time steps
-dt = T/(N_time-1); print(f'dt = {dt}') # Time step
+
+# Discretisation
+N_space = 11 # Number of space steps
+advection = "upwind" # or "central" or "blended"
+N_time = 11 # Number of time steps
 time = np.linspace(0.0, T, N_time) # Time mesh
 
+# Physical parameters
 alpha = 0.5 # Fractional derivative order
-
 # phi = 0.5 * np.ones(N_space)  # Porosity (standard derviative coefficient)
-
 beta = 0.5 * np.ones(N_space)  # Fractional derivative coefficient
+nu = 0.1 * np.ones(N_space)  # Diffusion coefficient field
+vel = 1.1 * np.ones(N_space)  # Advection velocity field
 
-nu = 0.01 * np.ones(N_space)  # Diffusion coefficient field
+# Boundary conditions
+zetaL = 0.0 # Left boundary condition (Neumann coefficient)
+xiL = 1.0 # Left boundary condition (Dirichlet coefficient)
+zetaR = 1.0 # Left boundary condition (Neumann coefficient)
+xiR = 0.0 # Left boundary condition (Dirichlet coefficient)
+# NB Dirichlet values are imposed by the initial condition
 
-vel = 0.0 * np.ones(N_space)  # Advection velocity field
+# Initial conditions
+# initial_condition = lambda x: (1 - x) * x
+initial_condition = lambda x: x>0
 
+# Forcing
+# forcing = lambda t, x: np.outer((np.abs(xR-x)*np.exp(x))*(0.3 + np.sin(15*x)/4), np.exp(-t*0.5*np.sin(5*t)))
+forcing = lambda t, x: np.outer(0.0*x, 0.0*t)
+
+# %%
+# Setup the problem
+
+# Create Mesh
 mesh_x = np.linspace(xL, xR, N_space) # Space mesh
-
 dx = mesh_x[1]-mesh_x[0] # Space step
 
+# Diffusion
 d0 = -2.0 * nu
 d0[0]  = 0
 d0[-1] = 0
@@ -42,6 +59,7 @@ d2[0]  = 0
 L_diff = (1/np.square(dx))*sparse.diags([d0, d1, d2], [0, -1, 1]) # Laplacian
 
 
+# Advection
 d0 = vel
 d1 = vel[1:]
 d2 = vel[:-1]
@@ -50,74 +68,62 @@ d0[-1] = 0
 d1[-1] = 0
 d2[0]  = 0
 
-L_a_r = (1/dx)*sparse.diags([-d0,  d2], [0, 1]) # right advection
-L_a_l = (1/dx)*sparse.diags([ d0, -d1], [0, -1]) # left advection
-L_a_c = (1/dx)*sparse.diags([-d1,  d2], [-1, 1])/2 # Central advection
+L_a_r = -(1/dx)*sparse.diags([-d0,  d2], [0, 1]) # right advection
+L_a_l = -(1/dx)*sparse.diags([ d0, -d1], [0, -1]) # left advection
+L_a_c = -(1/dx)*sparse.diags([-d1,  d2], [-1, 1])/2 # Central advection
 
+# Assemble full matrix
 L = None
 
 if advection == "upwind":
-    '''WE ASSUME POSITIVE VELOCITY FOR THE MOMENT'''
-    # L_a = L_a_r + L_a_l
-    L_a = L_a_l
+    L_a = sparse.diags((vel>0)*1.0, 0)@L_a_l + sparse.diags((vel<0)*1.0, 0)@L_a_r
     L = L_diff + L_a
 elif advection == "central":
     L_a = L_a_c
     L = L_diff + L_a
 
-# M = sparse.diags(np.ones(N_space), 0)
+# Mass (time derivative) matrix
 M = np.eye(N_space)
-M[0,1] = 1e-6
-M[-1,-2] = 1e-6
+M[0,1] =  -zetaL/(zetaL-xiL*dx)
+M[-1,-2] = -zetaR/(zetaR+xiR*dx)
 M = sparse.csr_matrix(M)
 
-
+# Fractional derivative matrix
 B = sparse.diags(beta, 0) @ M
 
-u = np.zeros((N_space, N_time)) #(x, t)
-# f = np.zeros_like(u)
-f = f_fun(np.linspace(0.0, T, N_time), mesh_x)
+# %%
+# Time stepping
 
-# u[ :,0] = 1.0 + 0.0*mesh_x
-u[ :,0] = (1-mesh_x)*mesh_x
 
-# u[ 0,0] = 0.0
-# u[-1,0] = 0.0
-
+# useful definitions
+dt = T/(N_time-1); print(f'dt = {dt}') # Time step
 halpha = np.power(dt, 1-alpha)
+f = forcing(np.linspace(0.0, T, N_time), mesh_x)
+f[0,:] = 0.0
+f[-1,:] = 0.0
+b_fun = lambda k, alpha: (np.power(k+1, 1-alpha)-np.power(k, 1-alpha))/gamma(2-alpha)
 
+# Set solution vector and initial condition
+u = np.zeros((N_space, N_time)) #(x, t)
+u[ :,0] = initial_condition(mesh_x)
+
+# Time loop
 for n in range(1, N_time):
 
     bb = b_fun(n-np.arange(1,n+1), alpha)
-
     A = M + (bb[-1]*halpha)*B - dt*L
-
     y = np.sum(u[:,1:n], axis=1)
 
-    f1 = (dt * L) @ y
-    f2 = halpha * B * (u[:,1:n] @ bb[:-1])
-    f3 = (B*(((n*dt)**(1-alpha))/gamma(2-alpha)))*u[:,0]
+    f1 = (M + B*(((n*dt)**(1-alpha))/gamma(2-alpha)))@u[:,0]
+    f2 = - halpha * B @ (u[:,1:n] @ bb[:-1])
+    f3 = (dt * L) @ y
     f4 = dt*np.sum(f[:,1:n], axis=1)
-
-    # fBC = np.zeros(N_space-2)
-    # # diffusion terms
-    # fBC[0]  = dt*(u[0,0] + y[0])
-    # fBC[-1] = dt*(u[-1,0] + y[-1])
-    # if advection == "central":
-    #     # advection terms (central)
-    #     fBC[0]  += vel_eq*(u[0,0] + y[0])/2
-    #     fBC[-1] -= vel_eq*(u[-1,0] +  y[-1])/2
-    # elif advection == "upwind":
-    #     # advection terms (upwind)
-    #     fBC[0]  += vel_eq*(u[0,0] + y[0]) * (vel > 0)
-    #     fBC[-1] += vel_eq*(u[-1,0] +  y[-1]) * (vel < 0)
         
-
-    u[:,n] = sparse.linalg.spsolve(A, f1 - f2 + f3 + f4)
-    # u[0,n]  = u[0,0]
-    # u[-1,n] = u[-1,0]
+    u[:,n] = sparse.linalg.spsolve(A, f1 + f2 + f3 + f4)
 
 
+# %%
+# Saving solution
 
 # SAVING SOLUTIONsvd_2020_demeaned_id20240227115752
 if True:
@@ -136,8 +142,9 @@ if True:
     np.savez(f'./results/out.npz', t=time, u=u, f=f, params=params)
 
 
+# %%
+# Plotting
 
-# PLOTTING
 if True:
     import matplotlib.gridspec as gridspec
     import matplotlib.pyplot as plt
